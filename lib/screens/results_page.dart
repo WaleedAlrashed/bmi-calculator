8f5e26c36b742f200895c11ed198a3e86d7d368a
Waleed Alrashed <wmr1210@hotmail.com>
2019-11-05 21:22:13 +0200

    Added Flutter alerts to show an alert after finish calculating each time

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,6 +1,8 @@
 import 'package:bmi_calculator/components/ButtomButton.dart';
 import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../components/ReusableCard.dart';
 
@@ -8,6 +10,16 @@ class ResultsPage extends StatelessWidget {
   final String bmiResult;
   final String resultText;
   final String interpretation;
+
+  launchURL() async {
+    const url = 'https://waleedalrashed.wordpress.com/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,7 +60,48 @@ class ResultsPage extends StatelessWidget {
                       interpretation.toUpperCase(),
                       textAlign: TextAlign.center,
                       style: kBodyTextStyle,
-                    )
+                    ),
+                    RaisedButton(
+                        color: kBottomContainerColor,
+                        child: Text(
+                          'About the Dev',
+                          style: kBodyTextStyle,
+                        ),
+                        onPressed: () {
+                          Alert(
+                            context: context,
+                            type: AlertType.info,
+                            title: "BMI Calculator",
+                            desc: "Created with ❤ in Damascus, Syria",
+                            buttons: [
+                              DialogButton(
+                                child: Text(
+                                  "Cool",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                onPressed: () => Navigator.pop(context),
+                                color: Color.fromRGBO(0, 179, 134, 1.0),
+                              ),
+                              DialogButton(
+                                child: Text(
+                                  "Website",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                onPressed: () async {
+                                  launchURL();
+                                },
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromRGBO(116, 116, 191, 1.0),
+                                    Color.fromRGBO(52, 138, 199, 1.0)
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ).show();
+                        })
                   ],
                 ),
               ),
