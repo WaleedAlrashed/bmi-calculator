1c76cb5cc73575abdad448068491e9dc566cf7bd
Waleed Alrashed <wmr1210@hotmail.com>
2020-09-26 19:34:21 +0300

    - Added framy_annotiation - Added github actions ci.yml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_alert/easy_alert.dart';
 import 'package:flutter/material.dart';
+import 'package:framy_annotation/framy_annotation.dart';
 
 import 'screens/input_page.dart';
 
@@ -13,13 +14,14 @@ void main() => runApp(
       ),
     );
 
+@FramyApp()
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF292B36),
       ),
       home: InputPage(),
     );
